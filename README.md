76dd9d09df953d5f501ba1ff2848a1dd39f95a17
digitaltk <tatendamakand@icloud.com>
2020-10-05 11:35:01 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ji2aoa
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ji2aoa)
---
+++ src/data.js
@@ -0,0 +1,14 @@
+const userList = [
+    {
+        id: 1,
+        name: 'Tatenda',
+        username: 'Tatenda Makandigona'
+    },
+    {
+        id: 2,
+        name: 'kurt',
+        username: 'Kurt Farao'
+    }
+];
+
+export default userList;
